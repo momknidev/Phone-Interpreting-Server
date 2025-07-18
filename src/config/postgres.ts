f7830d9fc4f01ccfe5350a51729cce3515286c4a
Abdul Waqar <abdulwaqar844@gmail.com>
2025-07-18 19:24:57 +0500

    updated postgress config

--- src/config/postgres.ts
+++ src/config/postgres.ts
@@ -5,6 +5,12 @@ import "dotenv/config";
 
 export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    // connectionTimeoutMillis: 5000, // Increase connection timeout if the connection is slow
+    // idleTimeoutMillis: 10000, // Increase idle timeout to prevent disconnections
+    // max: 20, // Set a higher maximum number of connections
+    keepAlive: true, // Keep the connection alive
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false, // Use SSL in production
+
 });
 
 export const db = drizzle(pool)
