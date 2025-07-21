a9f63313cd34d13104b39c8b127be3a5f9b7a279
Abdul Waqar <abdulwaqar844@gmail.com>
2025-07-21 21:58:24 +0500

    updated drizzle database connection function

--- src/boot/connectToPostgres.ts
+++ src/boot/connectToPostgres.ts
@@ -1,34 +1,19 @@
-import { db, pool } from '../config/postgres';
+import { db } from '../config/postgres';
 import { logger } from '../config/logger';
 
 export const connectToPostgres = async () => {
-    try {
-        logger.info("postgres:connecting:start");
+  try {
+    logger.info('postgres:connecting:start');
 
-        const result = await pool.query("SELECT NOW()");
-        if (result) {
-            logger.info("postgres:connecting:completed");
-        }
+    const result = await db.execute('select 1');
 
-        // Setup shutdown handler
-        process.on("SIGTERM", async () => {
-            await pool.end();
-            logger.info("postgres:connection:closed");
-        });
-
-        return db;
-    } catch (error) {
-        logger.error(`postgres:connecting:failed - ${error}`);
-        throw error;
+    if (result) {
+      logger.info('postgres:connecting:completed');
     }
-};
 
-export const disconnectFromPostgres = async () => {
-    try {
-        await pool.end();
-        logger.info("postgres:disconnected");
-    } catch (error) {
-        logger.error(`postgres:disconnect:failed - ${error}`);
-        throw error;
-    }
+    return db;
+  } catch (error) {
+    logger.error(`postgres:connecting:failed - ${error}`);
+    throw error;
+  }
 };
--- src/config/postgres.ts
+++ src/config/postgres.ts
@@ -1,16 +1,7 @@
 /* eslint-disable @typescript-eslint/quotes */
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
 import "dotenv/config";
 
-export const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    // connectionTimeoutMillis: 5000, // Increase connection timeout if the connection is slow
-    // idleTimeoutMillis: 10000, // Increase idle timeout to prevent disconnections
-    // max: 20, // Set a higher maximum number of connections
-    keepAlive: true, // Keep the connection alive
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false, // Use SSL in production
+const db = drizzle(process.env.DATABASE_URL ||'');
 
-});
-
-export const db = drizzle(pool)
+export { db };
