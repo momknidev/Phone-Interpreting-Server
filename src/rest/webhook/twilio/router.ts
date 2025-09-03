3dbdeadf2fb5249099b1a20db44d0e4401503e31
Waqar <abdulwaqar844@gmail.com>
2025-09-03 18:58:51 +0500

    Revert "added no credit logic"
    This reverts commit 0599a92ad9c47c08608dab930aa692a03f3a75d2.

--- src/rest/webhook/twilio/router.ts
+++ src/rest/webhook/twilio/router.ts
@@ -3,27 +3,62 @@ import * as controller from './controller';
 
 const twilioRouter = Router();
 
-twilioRouter.post('/call', controller.call);
-twilioRouter.post('/requestCode', controller.requestCode);
+twilioRouter.post(
+    '/call',
+    controller.call,
+);
+twilioRouter.post(
+    '/requestCode',
+    controller.requestCode,
+);
 
-twilioRouter.post('/validateCode', controller.validateCode);
+twilioRouter.post(
+    '/validateCode',
+    controller.validateCode,
+);
 
-twilioRouter.post('/requestSourceLanguage', controller.requestSourceLanguage);
+twilioRouter.post(
+    '/requestSourceLanguage',
+    controller.requestSourceLanguage,
+);
 
-twilioRouter.post('/validateSourceLanguage', controller.validateSourceLanguage);
-twilioRouter.post('/requestTargetLanguage', controller.requestTargetLanguage);
+twilioRouter.post(
+    '/validateSourceLanguage',
+    controller.validateSourceLanguage,
+);
+twilioRouter.post(
+    '/requestTargetLanguage',
+    controller.requestTargetLanguage,
+);
 
-twilioRouter.post('/validateTargetLanguage', controller.validateTargetLanguage);
+twilioRouter.post(
+    '/validateTargetLanguage',
+    controller.validateTargetLanguage,
+);
 
-twilioRouter.post('/callInterpreter', controller.callInterpreter);
+twilioRouter.post(
+    '/callInterpreter',
+    controller.callInterpreter,
+);
 
-twilioRouter.post('/machineDetectionResult', controller.machineDetectionResult);
+twilioRouter.post(
+    '/machineDetectionResult',
+    controller.machineDetectionResult,
+);
 
-twilioRouter.post('/callStatusResult', controller.callStatusResult);
+twilioRouter.post(
+    '/callStatusResult',
+    controller.callStatusResult,
+);
 
-twilioRouter.post('/conferenceStatusResult', controller.conferenceStatusResult);
+twilioRouter.post(
+    '/conferenceStatusResult',
+    controller.conferenceStatusResult,
+);
 
-twilioRouter.post('/noAnswer', controller.noAnswer);
-twilioRouter.post('/creditExhausted', controller.creditExhausted);
+twilioRouter.post(
+    '/noAnswer',
+    controller.noAnswer,
+);
 
 export { twilioRouter };
